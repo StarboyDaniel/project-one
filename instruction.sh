5c0e10fb341adfa8a2d483922f8b9fc34f5a6c2e
Ubuntu <ubuntu@ip-172-31-36-205.us-east-2.compute.internal>
2023-09-25 03:00:59 +0000

    Created a bash script

---
+++ instruction.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#OWNER: DANIEL ADDISON
+#DATE:SEPTEMBER 24 2023
+#DESCRIPTION: Introduction into automation using bash shell scriptiing.
+#contain various linux command to test run bash scripting.
+
+
+
+
+
+#Greetings from the machine
+echo "Hi there! I hope you are doing well today"
+sleep 5
+clear
+
+
+
+#This command is meant to reveal the current user signed into thsi machine
+echo "You are the user called" && whoami
+#whoami
+sleep 5
+clear
+
+
+
+#This command reveals the present working directory
+echo "You are currently working in the directory shown below"
+pwd
+sleep 5
+clear
+
+
